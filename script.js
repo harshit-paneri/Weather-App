38baf3624288b538b966f8918603121cc3388d0a
Harshit Paneri <harshitpanari@gmail.com>
2022-12-11 14:17:10 +0530

    backgroud add/ garbage remove

--- script.js
+++ script.js
@@ -6,31 +6,31 @@ const options = {
   },
 };
 const getWeather = (city) => {
-    cityName.innerHTML= city;
-fetch(
-  'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city,
-  options
-)
-  .then((response) => response.json())
-  .then((response) => {
-    console.log(response);
-    wind_speed.innerHTML = response.wind_speed
-    wind_degrees.innerHTML = response.wind_degrees
-    temp.innerHTML = response.temp
-    humidity.innerHTML = response.humidity
-    sunset.innerHTML = response.sunset
-    min_temp.innerHTML = response.min_temp
-    cloud_pct.innerHTML = response.cloud_pct
-    feels_like.innerHTML = response.feels_like
-    sunrise.innerHTML = response.sunrise
-    max_temp.innerHTML = response.max_temp
-  })
-  .catch((err) => console.error(err));
-}
+  cityName.innerHTML = city;
+  fetch(
+    "https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=" + city,
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+      wind_speed.innerHTML = response.wind_speed;
+      wind_degrees.innerHTML = response.wind_degrees;
+      temp.innerHTML = response.temp;
+      humidity.innerHTML = response.humidity;
+      sunset.innerHTML = response.sunset;
+      min_temp.innerHTML = response.min_temp;
+      cloud_pct.innerHTML = response.cloud_pct;
+      feels_like.innerHTML = response.feels_like;
+      sunrise.innerHTML = response.sunrise;
+      max_temp.innerHTML = response.max_temp;
+    })
+    .catch((err) => console.error(err));
+};
 
-submit.addEventListener("click",(e)=>{
-    e.preventDefault();
-    getWeather(city.value);
-})
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  getWeather(city.value);
+});
 
 getWeather("Udaipur");
